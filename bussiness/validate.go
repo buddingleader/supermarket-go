7bb37c8b6e0d801c04fbc3ba604e3e4eb1e7e997
wangff <314130948@qq.com>
2018-05-19 09:46:45 +0800

    8位以上不含是条形码，小于等于8位是金额

--- bussiness/validate.go
+++ bussiness/validate.go
@@ -4,7 +4,7 @@ import "strconv"
 
 // checkBarcode 校验标准条形码
 func checkBarcode(str string) (int64, bool) {
-	if len(str) != 13 {
+	if len(str) <= 8 {
 		return 0, false
 	}
 	v, err := strconv.ParseInt(str, 10, 64)
@@ -16,6 +16,9 @@ func checkBarcode(str string) (int64, bool) {
 
 // checkPrice 校验价格
 func checkPrice(str string) (float64, bool) {
+	if len(str) > 8 {
+		return 0, false
+	}
 	v, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		return 0.00, false
